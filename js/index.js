2464e93c005b458f9d42d63dcbb9aabb112303dc
RealWillBrooks <wbrooks107@gmail.com>
2019-06-17 18:13:10 -0500

    Finished MVP, working on Stretch

--- js/index.js
+++ js/index.js
@@ -57,14 +57,6 @@ navLinks.forEach((link, index) => {
   link.textContent = siteContent ['nav'][`nav-item-${index+1}`]
 })
 
-
-
-const allNavLinks = document.querySelectorAll('nav a');
-
-allNavLinks.forEach((link) => {
-  link.style.color = 'black';
-})
-
 //------------------------- CTA SECTION HERE --------------------------------------------------//
 domIsAwesomeH1 = document.querySelector('.cta-text h1').innerHTML = siteContent['cta']['h1'];
 domIsAwesomeButton = document.querySelector('.cta-text button').innerHTML = siteContent['cta']['button'];
@@ -72,3 +64,65 @@ domIsAwesomeImg = document.querySelector('.cta img').src = siteContent['cta']['i
 
 //---------------------- MAIN CONTENT -------------------------------------------------------//
 
+// let Contentone = document.getElementsByClassName('text-content')[0]
+// Contentone.getElementsByTagName('h4')[0].innerHTML = siteContent['main-content']['features-h4']
+// Contentone.getElementsByTagName('p')[0].innerHTML = siteContent['main-content']["features-content"]
+
+let firstContent = document.getElementsByClassName('text-content')[0]
+firstContent.getElementsByTagName('h4')[0].innerHTML = siteContent['main-content']['features-h4']
+firstContent.getElementsByTagName('p')[0].innerHTML = siteContent['main-content']["features-content"]
+
+// let ContentTwo = document.getElementsByClassName('text-content')[1]
+// ContentTwo.getElementsByTagName('h4')[1].innerHTML = siteContent['main-content']['about-h4']
+// ContentTwo.getElementsByTagName('p')[1].innerHTML = siteContent['main-content']["features-content"]
+
+let secondContent = document.getElementsByClassName('text-content')[1]
+secondContent.getElementsByTagName('h4')[0].innerHTML = siteContent['main-content']['about-h4']
+secondContent.getElementsByTagName('p')[0].innerHTML = siteContent['main-content']["features-content"]
+
+//-------MIDDLE IMAGE------//
+
+let nextLogo = document.getElementById('middle-img')
+nextLogo.setAttribute('src',siteContent['main-content']['middle-img-src'])
+//------- MAIN CONTENT (BOTTOM)----//
+let thirdContent = document.getElementsByClassName('text-content')[2]
+thirdContent.getElementsByTagName('h4')[0].innerHTML = siteContent['main-content']['services-h4']
+thirdContent.getElementsByTagName('p')[0].innerHTML = siteContent['main-content']["services-content"]
+
+let forthContent = document.getElementsByClassName('text-content')[3]
+forthContent.getElementsByTagName('h4')[0].innerHTML = siteContent['main-content']['product-h4']
+forthContent.getElementsByTagName('p')[0].innerHTML = siteContent['main-content']["product-content"]
+
+let fifthContent = document.getElementsByClassName('text-content')[4]
+fifthContent.getElementsByTagName('h4')[0].innerHTML = siteContent['main-content']['vision-h4']
+fifthContent.getElementsByTagName('p')[0].innerHTML = siteContent['main-content']["vision-content"]
+
+//------------------ CONTACT ---------------------//
+
+const contacth4 = document.querySelectorAll("h4")[5];
+contacth4.innerText = siteContent["contact"]["contact-h4"];
+
+const address = document.querySelectorAll("p")[5];
+address.innerText = siteContent["contact"]["address"];
+
+const phone = document.querySelectorAll("p")[6];
+phone.innerText = siteContent["contact"]["phone"];
+
+const email = document.querySelectorAll("p")[7];
+email.innerText = siteContent["contact"]["email"];
+
+//----------------- FOOTER ------------------//
+
+const copyright = document.getElementsByTagName("p")[8];
+copyright.innerText = siteContent['footer']['copyright']
+
+//----------------- STRETCH ----------------//
+
+const navigation = document.querySelector ('nav')
+const newanchor = document.createElement ('')
+
+const allNavLinks = document.querySelectorAll('nav a');
+
+allNavLinks.forEach((link) => {
+  link.style.color = 'navy';
+})
